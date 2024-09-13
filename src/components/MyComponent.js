5f2c16b3c2897f917991581b06588e777576029a
banhkute200 <anhquanvo911@gmail.com>
2024-09-13 09:13:02 +0700

    video 23

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import UserInfor from './UserInfor'
+import DisplayInfor from './DisplayInfor';
 
 class MyComponent extends React.Component {
-
     //JSX
     render() {
+        const myInfor = ['av', 'a', 'v']
         return (
             <div>
                 <UserInfor />
+                <br /> <br />
+                <DisplayInfor name={'Vo Anh Quan'} age={21} myInfor={myInfor} />
             </div>
 
         );
