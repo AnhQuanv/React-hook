7d0f0161f26c8b45780b2f8f2412c24b7e1fa58d
banhkute200 <anhquanvo911@gmail.com>
2024-09-13 20:29:00 +0700

    video 27

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './DisplayInfor.scss'
 
 class DisplayInfor extends React.Component {
 
@@ -17,7 +18,7 @@ class DisplayInfor extends React.Component {
         const { listUsers } = this.props
         // pros => properties
         return (
-            <div>
+            <div className="display-infor-container">
                 <div>
                     <span onClick={(event) => { this.handleShowHide(event) }}>
 
@@ -29,7 +30,7 @@ class DisplayInfor extends React.Component {
                         {listUsers.map((user) => {
                             return (
                                 <div key={user.id} className={+user.age > 18 ? 'blue' : 'red'}>
-                                    <div>My name's {user.name}</div>
+                                    <div style={{ color: 'yellow', paddingTop: '50px' }}>My name's {user.name}</div>
                                     <div>My age's {user.age}</div>
                                     <hr />
                                 </div>
