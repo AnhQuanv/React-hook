0e81f2e4a26d1777ab61cbb2650b136833b2cf77
banhkute200 <anhquanvo911@gmail.com>
2024-09-23 18:38:03 +0700

    video 83 - Data processing on the front end

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getDataQuiz } from "../../services/apiServices";
+import _ from 'lodash'
 
 
 const DetailQuiz = (props) => {
@@ -9,11 +10,33 @@ const DetailQuiz = (props) => {
 
     useEffect(() => {
         fetchQuestions();
-    }, [quizId])
+    }, [quizId]);
 
     const fetchQuestions = async () => {
         let res = await getDataQuiz(quizId);
         console.log('check questions: ', res);
+        if (res && res.EC === 0) {
+            let raw = res.DT;
+            let data = _.chain(raw)
+                // Group the elements of Array based on `color` property
+                .groupBy("id")
+                // `key` is group's name (color), `value` is the array of objects
+                .map((value, key) => {
+                    let answers = [];
+                    let questionDescription, image = null;
+                    value.forEach((item, index) => {
+                        if (index === 0) {
+                            questionDescription = item.description;
+                            image = item.image;
+                        }
+                        answers.push(item.answers);
+                    })
+                    return { questionId: key, answers, questionDescription, image };
+                }
+                )
+                .value();
+            console.log(data);
+        }
     }
 
     return (
