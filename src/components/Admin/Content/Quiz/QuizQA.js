784bf261a9ddaa7a05bce10bef78cf3e045cbe06
banhkute200 <anhquanvo911@gmail.com>
2024-10-01 18:08:08 +0700

    video 100 - Design Update/Delete - Questions/Answers

---
+++ src/components/Admin/Content/Quiz/QuizQA.js
@@ -0,0 +1,359 @@
+import React, { useEffect, useState } from 'react'
+import Select from 'react-select';
+import './QuizQA.scss'
+import { FaPlus, FaMinus } from "react-icons/fa";
+import { FiMinusCircle, FiPlusCircle } from "react-icons/fi";
+import { RiImageAddFill } from 'react-icons/ri';
+import { v4 as uuidv4 } from 'uuid';
+import _ from 'lodash';
+import Lightbox from "react-awesome-lightbox";
+import { getAllQuizForAdmin, postCreateNewAnswerForQuiz, postCreateNewQuestionForQuiz } from '../../../../services/apiServices';
+import { toast } from 'react-toastify';
+
+const QuizQA = (props) => {
+
+    const initQuestion =
+        [
+            {
+                id: uuidv4(),
+                description: '',
+                imageFile: '',
+                imageName: '',
+                isValidQuestion: true,
+                answers: [
+                    {
+                        id: uuidv4(),
+                        description: '',
+                        isCorrect: false,
+                        isValidAnswer: true
+                    }
+                ]
+            }
+
+        ]
+
+    const [questions, setQuestions] = useState(initQuestion);
+
+    const [isPreviewImage, setIsPreviewImage] = useState(false);
+
+    const [dataImagePreview, setDataImagePreview] = useState({
+        title: '',
+        url: ''
+    });
+
+    const [listQuiz, setListQuiz] = useState([]);
+
+    const [selectedQuiz, setSelectedQuiz] = useState({});
+
+
+    useEffect(() => {
+        fetchQuiz();
+    }, [])
+
+    const fetchQuiz = async () => {
+        let res = await getAllQuizForAdmin();
+        if (res && res.EC === 0) {
+            let newQuiz = res.DT.map(item => {
+                return {
+                    value: item.id,
+                    label: `${item.id} - ${item.description}`
+                };
+            })
+            setListQuiz(newQuiz);
+        }
+    }
+
+    const handleAddRemoveQuestion = (type, id) => {
+        console.log("check ", type, id)
+        if (type === 'ADD') {
+            const newQuestion = {
+                id: uuidv4(),
+                description: '',
+                imageFile: '',
+                imageName: '',
+                answers: [
+                    {
+                        id: uuidv4(),
+                        description: '',
+                        isCorrect: false
+                    }
+                ]
+            };
+
+            setQuestions([...questions, newQuestion]);
+        }
+        if (type === 'REMOVE') {
+            let questionClone = _.cloneDeep(questions);
+
+            questionClone = questionClone.filter(item => item.id !== id);
+            setQuestions(questionClone);
+
+        }
+    }
+
+
+
+    const handleAddRemoveAnswer = (type, questionId, AnswerId) => {
+        console.log("check ", type, questionId, AnswerId)
+        let questionsClone = _.cloneDeep(questions);
+
+        if (type === 'ADD') {
+            const newAnswer = {
+                id: uuidv4(),
+                description: '',
+                isCorrect: false
+            };
+
+            let index = questionsClone.findIndex(item => item.id === questionId);
+            questionsClone[index].answers.push(newAnswer)
+            setQuestions(questionsClone);
+        }
+        if (type === 'REMOVE') {
+            let index = questionsClone.findIndex(item => item.id === questionId);
+            questionsClone[index].answers = questionsClone[index].answers.filter(item => item.id !== AnswerId);
+            setQuestions(questionsClone);
+        }
+    }
+
+    const handleOnChange = (type, questionId, value) => {
+        if (type === 'QUESTION') {
+            let questionsClone = _.cloneDeep(questions);
+            let index = questionsClone.findIndex(item => item.id === questionId);
+            if (index > -1) {
+                questionsClone[index].description = value;
+                setQuestions(questionsClone);
+            }
+
+        }
+    }
+
+    const handleOnChangeFileQuestion = (questionId, event) => {
+        let questionsClone = _.cloneDeep(questions);
+        let index = questionsClone.findIndex(item => item.id === questionId);
+        if (index > -1 && event.target && event.target.files && event.target.files[0]) {
+            questionsClone[index].imageFile = event.target.files[0];
+            questionsClone[index].imageName = event.target.files[0].name;
+            setQuestions(questionsClone);
+        }
+    }
+
+    const handleAnswerQuestion = (type, answerId, questionId, value) => {
+        let questionsClone = _.cloneDeep(questions);
+        let index = questionsClone.findIndex(item => item.id === questionId);
+        if (index > -1) {
+            questionsClone[index].answers = questionsClone[index].answers.map(answer => {
+                if (answer.id === answerId) {
+                    if (type === 'CHECKBOX') {
+                        answer.isCorrect = value;
+                    }
+                    if (type === 'INPUT') {
+                        answer.description = value
+                    }
+                }
+                return answer;
+
+            })
+
+            setQuestions(questionsClone);
+        }
+    }
+
+
+    //submit quesstion
+
+    const handleSubmitQuestionForQuiz = async () => {
+
+        // validate 
+        if (_.isEmpty(selectedQuiz)) {
+            toast.error("Please choose a Quiz")
+            return;
+        }
+
+        //validate Answer
+        let isValid = true;
+        let indexQ = 0, indexA = 0;
+        for (let i = 0; i < questions.length; i++) {
+            for (let j = 0; j < questions[i].answers.length; j++) {
+                if (!questions[i].answers[j].description) {
+                    isValid = false;
+                    indexA = j;
+                    break;
+                }
+            }
+            indexQ = i;
+            if (isValid === false) {
+                break;
+            }
+        }
+
+        if (isValid === false) {
+            toast.error(`Not empty Answer ${indexA + 1} at Question ${indexQ + 1}`)
+            return;
+        }
+
+        //validate Question
+        let isValidQ = true;
+        let indexQ1 = 0;
+        for (let i = 0; i < questions.length; i++) {
+            if (!questions[i].description) {
+                isValidQ = false;
+                indexQ1 = i;
+                break;
+            }
+        }
+
+        if (isValidQ === false) {
+            toast.error(`Not empty description for Question ${indexQ1 + 1}`)
+            return;
+        }
+
+        for (const question of questions) {
+            const q = await postCreateNewQuestionForQuiz(+selectedQuiz.value, question.description, question.imageFile, question.id);
+
+            for (const answer of question.answers) {
+                await postCreateNewAnswerForQuiz(answer.description, answer.correct_answer, q.DT.id);
+            }
+        }
+
+        toast.success('Create question and answer success!');
+        setQuestions(initQuestion);
+    }
+
+    const handlePreivewImage = (questionId) => {
+        let questionsClone = _.cloneDeep(questions);
+        let index = questionsClone.findIndex(item => item.id === questionId);
+        if (index > -1) {
+            setDataImagePreview({
+                url: URL.createObjectURL(questionsClone[index].imageFile),
+                title: questionsClone[index].imageName
+            });
+            setIsPreviewImage(true);
+        }
+    }
+
+    return (
+        <div className='questions-container'>
+            <div className='add-new-question'>
+                <div className='col-6 form-group'>
+                    <label className='mb2-'>Select Quiz:</label>
+                    <Select
+                        defaultValue={selectedQuiz}
+                        onChange={setSelectedQuiz}
+                        options={listQuiz}
+                        placeholder={"Quiz type..."}
+                    />
+                </div>
+                <div className='mt-3 mb-2'>
+                    Add questions:
+                </div>
+                {
+                    questions && questions.length > 0
+                    && questions.map((question, index) => {
+                        return (
+                            <div key={question.id} className='q-main mb-4'>
+                                <div className='questions-content'>
+                                    <div className="form-floating description">
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            placeholder="name@example.com"
+                                            value={question.description}
+                                            onChange={(e) => handleOnChange('QUESTION', question.id, e.target.value)}
+                                        />
+                                        <label> Question {index + 1}'s description </label>
+                                    </div>
+                                    <div className='group-upload'>
+                                        <label htmlFor={`${question.id}`}>
+                                            <RiImageAddFill className='label-up' />
+                                        </label>
+                                        <input
+                                            id={`${question.id}`}
+                                            type='file'
+                                            hidden
+                                            onChange={(e) => handleOnChangeFileQuestion(question.id, e)}
+
+                                        />
+                                        <span>{question.imageName ?
+                                            <span style={{ cursor: 'pointer' }} onClick={() => handlePreivewImage(question.id)}>{question.imageName}</span>
+                                            :
+                                            '0 file is uploaded'}
+                                        </span>
+                                    </div>
+                                    <div className='btn-add'>
+                                        <span onClick={() => handleAddRemoveQuestion('ADD', '')}>
+                                            <FaPlus className='icon-add' />
+                                        </span>
+
+                                        {questions.length > 1 &&
+                                            <span onClick={() => handleAddRemoveQuestion('REMOVE', question.id)}>
+                                                <FaMinus className='icon-remove' />
+                                            </span>
+                                        }
+
+                                    </div>
+                                </div>
+
+                                {
+                                    question.answers && question.answers.length > 0
+                                    && question.answers.map((answer, index) => {
+                                        return (
+                                            <div key={answer.id} className='answers-content'>
+                                                <input
+                                                    className="form-check-input iscorrect"
+                                                    type="checkbox"
+                                                    checked={answer.isCorrect}
+                                                    onChange={(e) => handleAnswerQuestion('CHECKBOX', answer.id, question.id, e.target.checked)}
+                                                />
+                                                <div className="form-floating answer-name">
+                                                    <input
+                                                        type="text"
+                                                        className="form-control"
+                                                        placeholder="name@example.com"
+                                                        value={answer.description}
+                                                        onChange={(e) => handleAnswerQuestion('INPUT', answer.id, question.id, e.target.value)}
+                                                    />
+                                                    <label >Answer{index + 1}</label>
+                                                </div>
+                                                <div className='btn-group'>
+                                                    <span onClick={() => handleAddRemoveAnswer('ADD', question.id)}>
+                                                        <FiPlusCircle className='icon-add' />
+                                                    </span>
+                                                    {question.answers.length > 1 &&
+                                                        <span onClick={() => handleAddRemoveAnswer('REMOVE', question.id, answer.id)} >
+                                                            <FiMinusCircle className='icon-remove' />
+                                                        </span>
+                                                    }
+                                                </div>
+                                            </div>
+                                        )
+                                    })
+                                }
+
+
+
+                            </div>
+                        )
+                    })
+                }
+                {
+                    questions && questions.length > 0 &&
+                    <div>
+                        <button onClick={() => handleSubmitQuestionForQuiz()} className='btn btn-warning'>Save Question</button>
+                    </div>
+                }
+
+                {
+                    isPreviewImage === true &&
+                    <Lightbox
+                        image={dataImagePreview.url}
+                        title={dataImagePreview.title}
+                        onClose={() => setIsPreviewImage(false)}
+                    ></Lightbox>
+                }
+
+            </div>
+        </div >
+    )
+}
+
+export default QuizQA
