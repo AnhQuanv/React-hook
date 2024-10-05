9d5d52d39206334ea663f53882cd083c483b7547
banhkute200 <anhquanvo911@gmail.com>
2024-10-05 10:46:36 +0700

    video 110 - Design Dashboard

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -4,6 +4,8 @@ import './Admin.scss';
 import { useState } from "react";
 import { Outlet } from "react-router-dom"
 import PerfectScrollbar from 'react-perfect-scrollbar';
+import { NavDropdown } from "react-bootstrap";
+import Language from "../Header/Language";
 
 
 
@@ -16,8 +18,18 @@ const Admin = (props) => {
             </div>
             <div className="admin-content">
                 <div className="admin-header">
-                    <FaBars onClick={() => setCollapsed(!collapsed)} />
+                    <span onClick={() => setCollapsed(!collapsed)}>
+                        <FaBars className="leftside" />
+                    </span>
+                    <div className="rightside">
+                        <Language />
+                        <NavDropdown title="Settings" id="basic-nav-dropdown">
+                            <NavDropdown.Item>Profile</NavDropdown.Item>
+                            <NavDropdown.Item> Log out </NavDropdown.Item>
+                        </NavDropdown>
+                    </div>
                 </div>
+
                 <div className="admin-main">
                     <PerfectScrollbar>
                         <Outlet />
--- src/components/Admin/Content/DashBoard.js
+++ src/components/Admin/Content/DashBoard.js
@@ -1,7 +1,73 @@
+import { Bar, BarChart, CartesianGrid, Legend, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
+import './DashBoard.scss';
+
 const DashBoard = (props) => {
+
+    const data = [
+        {
+            "name": "Page A",
+            "uv": 4000,
+            "pv": 2400
+        },
+        {
+            "name": "Page B",
+            "uv": 3000,
+            "pv": 1398
+        },
+        {
+            "name": "Page C",
+            "uv": 2000,
+            "pv": 9800
+        },
+        {
+            "name": "Page D",
+            "uv": 2780,
+            "pv": 3908
+        },
+        {
+            "name": "Page E",
+            "uv": 1890,
+            "pv": 4800
+        },
+        {
+            "name": "Page F",
+            "uv": 2390,
+            "pv": 3800
+        },
+        {
+            "name": "Page G",
+            "uv": 3490,
+            "pv": 4300
+        }
+    ]
+
     return (
-        <div>
-            dash board
+        <div className="dashboard-container">
+            <div className='title'>
+                Analytics DashBoard
+
+            </div>
+            <div className='content'>
+                <div className='c-left'>
+                    <div className='child'>1</div>
+                    <div className='child'>2</div>
+                    <div className='child'>3</div>
+                    <div className='child'>4</div>
+                </div>
+                <div className='c-right'>
+                    <ResponsiveContainer width="100%" height="100%">
+                        <BarChart data={data}>
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis dataKey="name" />
+                            <YAxis />
+                            <Tooltip />
+                            <Legend />
+                            <Bar dataKey="pv" fill="#8884d8" />
+                            <Bar dataKey="uv" fill="#82ca9d" />
+                        </BarChart>
+                    </ResponsiveContainer>
+                </div>
+            </div>
         </div>
     )
 }
